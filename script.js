1429aecc642947a78b5b83a1b7021a4e109e4092
isabeela <31148780+isabeela@users.noreply.github.com>
2024-01-25 10:43:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ function formatarData(inputValue) {
             "<td>" + formatarSalario(parseFloat(salarioMensal)) + "</td>" +
             "</tr>";
 
-        relatorioFinal += "<tr><td colspan='2'>______</td></tr>";
+        relatorioFinal += "<tr><td colspan='2'>___</td></tr>";
 
         // Avance para o próximo mês
         currentDate.setMonth(currentDate.getMonth() + 1);
